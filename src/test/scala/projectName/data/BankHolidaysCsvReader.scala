d9505f53ba6f1b5061d61fdbf612b0e841b890fe
Kenny Vo <kennyvo@Kennys-MacBook-Pro.local>
2019-11-11 11:49:15 +0000

    Add read/write capability

---
+++ src/test/scala/projectName/data/BankHolidaysCsvReader.scala
@@ -0,0 +1,16 @@
+package projectName.data
+
+import java.time.LocalDate
+
+import scala.io.Source
+
+object BankHolidaysCsvReader extends Csv {
+
+  def readBankHolidays(fileName: String): List[LocalDate] = {
+    val fileLines: List[String] = readLines(fileName)
+
+    fileLines.map(line => LocalDate.parse(line.replaceAll(",", "")))
+  }
+
+  val bankHolidays: List[LocalDate] = BankHolidaysCsvReader.readBankHolidays(filePath("bank_holidays.csv"))
+}
---
+++ src/test/scala/projectName/data/Csv.scala
@@ -0,0 +1,13 @@
+package projectName.data
+
+import java.io.File
+
+import scala.io.Source
+
+trait Csv {
+
+  def filePath(fileName: String): String = new File(s"src/test/resources/$fileName").getCanonicalPath
+
+  def readLines(fileName: String): List[String] = Source.fromFile(fileName).getLines().toList
+
+}
