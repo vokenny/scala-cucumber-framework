9b3feb0bd91f4dac0040d3f79d6ed1056be43323
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2020-10-22 22:59:46 +0100

    Add TODO

--- src/test/scala/projectName/pages/AccountPage.scala
+++ src/test/scala/projectName/pages/AccountPage.scala
@@ -3,6 +3,7 @@ package projectName.pages
 import projectName.pages.traits.BasePage
 import projectName.utils.BaseUrls.automationBaseUrl
 
+// TODO: Use the-internet Heroku for test automation practice
 object AccountPage extends BasePage {
 
   val url: String = s"$automationBaseUrl/index.php?controller=my-account"
