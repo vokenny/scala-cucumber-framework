487b59f23fd73ed1ff7ee1c5211fa225af27c8fa
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2020-10-24 22:30:23 +0100

    Reduce code for StringGenerator

--- src/test/scala/scalaCucumberFramework/data/StringGenerator.scala
+++ src/test/scala/scalaCucumberFramework/data/StringGenerator.scala
@@ -11,24 +11,9 @@ object StringGenerator extends App with Csv {
 
   def writeStringsToCsv(filePath: String, num: Int): Unit = {
     val bw: BufferedWriter = new BufferedWriter(new FileWriter(filePath))
-
-    @tailrec
-    def writeStrings(remainingList: List[String]): Unit = {
-      remainingList match {
-        case Nil => // No action
-        case h :: Nil => bw.write(h)
-        case h :: tail =>
-          bw.write(h + ",\n")
-          writeStrings(tail)
-      }
-    }
-
-
     val stringList: List[String] = List.fill(num)(generateString())
-    writeStrings(stringList)
-    // Alternatively, shorter but less performant code:
-    //stringList.init.foreach(e => bw.write(e + ",\n"))
-    //bw.write(stringList.last)
+    stringList.init.foreach(e => bw.write(e + ",\n"))
+    bw.write(stringList.last)
     bw.close()
   }
 
